d756ea4f214d9237012b9d68c336471ceda86460
Shared Ordaz <sharedordaz@live.com.mx>
2023-03-07 00:51:27 -0600

    Changed swagger

--- backend/swagger.js
+++ backend/swagger.js
@@ -8,13 +8,12 @@ const doc = {
   host: "lesson2-backend2-sharedordaz.onrender.com",
   basePath: "/",
   schemes: ['http', 'https'],
-  produces: ['application/json'],
-  licence : {
+  produces: ['application/json']
+};
+/*licence : {
   name: "GPL3",
   url: "https://www.gnu.org/licenses/gpl-3.0.html"
-  }
-};
-
+  }*/
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
